d00810730ca9c34f3be2d253e95f044350b4fbf2
david.sylvestre.1@ens.etsmtl.ca <david.sylvestre.1@ens.etsmtl.ca>
2017-10-07 15:54:12 -0400

    States

--- state.py
+++ state.py
@@ -1,8 +1,11 @@
 from action import *
 
+class State:
+    pass
+
 class StateMachine:
     def __init__(self):
-        self.current = None
+        self.current = StateGatherRessources()
 
     def run(self, player, map, otherPlayers):
         if self.current is not None:
@@ -11,3 +14,46 @@ class StateMachine:
             action = create_move_action(Point(-1, -1))
 
         return action
+
+pointConversion ⁼ {'^': Point(0, -1), '<': Point(-1, 0), '>': Point(1, 0), 'v': Point(0, 1) }
+
+def getNextMove(playerPos, path):
+    return playerPos + pointConversion[path[0]]
+
+class StateRoam(State):
+    def run(player, gameMap, other_players):
+
+        if gameMap.contains(TileContent.Resource):
+            return StateGatherRessources()
+
+        path = findShortestPathToType(gameMap, player.Position, -1) #unknown tile
+        return create_move_action(getNextMove(player.Position, path))
+
+
+class StateGoToHouse(State):
+    def run(player, gameMap, other_players):
+
+        if player.Position == player.HouseLocation:
+            return StateGatherRessources()
+
+        path = findShortestPath(gameMap, player.Position, player.HouseLocation)
+        return create_move_action(getNextMove(player.Position, path))
+
+#gather closest ressource point from the player
+class StateGatherRessources(State):
+
+    def run(player, gameMap, other_players):
+
+        if player.CarriedResources >= player.CarryingCapacity:
+            return StateGoToHouse()
+
+        path = findShortestPathToType(gameMap, player.Position, TileContent.Resource)
+
+        if path is None:
+            return StateRoam()
+
+        closestResource = gameMap.touch(TileContent.Resource)
+        if len(path) == 0:
+            return create_collect_action(closestResource)
+        else:
+            return create_move_action(getNextMove(player.Position, path))
